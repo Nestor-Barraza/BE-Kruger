00d2d196b6a3c98f13dc34bd77cb714b9bbd40e3
克里斯托弗·霍克 <nestorbarrazaotalvarez@hotmail.com>
2024-05-15 00:39:17 -0500

    feat: change base url

--- src/app.ts
+++ src/app.ts
@@ -7,23 +7,21 @@ import { errorHandler } from "./middlewares/errors";
 import { authMiddleware } from "./middlewares/session";
 
 const app: Application = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 RegisterRoutes(app);
 
 const swaggerSpec = require("./utils/swagger.json");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorHandler);
-
 app.use(authMiddleware);
 
 app.listen(port, () => {
-  const swaggerUrl = `http://localhost:${port}/api-docs`.bgBlue;
+  const swaggerUrl = `${process.env.BASE_URL}/api-docs`.bgBlue;
   console.log(`Server listening on port ${port}`.cyan);
   console.log(
     `Swagger documentation available at:`.bgGreen +
